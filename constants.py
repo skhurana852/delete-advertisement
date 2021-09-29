29edffc7a4f86b2ef04aece67a06a5cfc58b9386
Saransh Khurana <saranshkhurana5@gmail.com>
2021-09-29 16:28:32 +0530

    added constants

---
+++ constants.py
@@ -0,0 +1,7 @@
+BASE_URI = "https://gmail.googleapis.com/gmail/v1/users"
+DELETE_MSG = "Some Mails Deleted"
+ERR_MSG = "There was a problem!"
+ACCESS_TOKEN = {YOUR_ACCESS_TOKEN}
+KEYWORDS = [
+    "unsubscribe", "Unsubscribe", "Unsubscribe:", "|unsubscribe"
+]
