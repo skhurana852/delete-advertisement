c16b00ced6109ffbb31f0786d72f96efdbe52ad8
Saransh Khurana <saranshkhurana5@gmail.com>
2021-09-30 12:52:20 +0530

    updated constants

--- constants.py
+++ constants.py
@@ -1,7 +1,5 @@
 BASE_URI = "https://gmail.googleapis.com/gmail/v1/users"
-DELETE_MSG = "Some Mails Deleted"
+DELETE_MSG = "Mails Deleted"
 ERR_MSG = "There was a problem!"
 ACCESS_TOKEN = {YOUR_ACCESS_TOKEN}
-KEYWORDS = [
-    "unsubscribe", "Unsubscribe", "Unsubscribe:", "|unsubscribe"
-]
+KEYWORD = "Zomato"
